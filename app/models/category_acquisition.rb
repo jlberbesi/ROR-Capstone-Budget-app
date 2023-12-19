11ec953a1be4d58c9fa097f6600ecb8b2b475a6b
José Berbesi <88105477+jlberbesi@users.noreply.github.com>
2023-12-18 20:10:42 -0500

    Linters!!!

--- app/models/category_acquisition.rb
+++ app/models/category_acquisition.rb
@@ -1,4 +1,4 @@
 class CategoryAcquisition < ApplicationRecord
-    belongs_to :category, class_name: 'Category'
-    belongs_to :acquisition, class_name: 'Acquisition'
-  end
+  belongs_to :category, class_name: 'Category'
+  belongs_to :acquisition, class_name: 'Acquisition'
+end
